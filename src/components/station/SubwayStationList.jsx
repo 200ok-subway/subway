c9c413140d604f4691dd42c1f15da4b50c0057e0
siromiji <mnmn82018282@gmail.com>
2025-10-30 09:35:15 +0900

    251030 서치바 코드 수정

--- src/components/station/SubwayStationList.jsx
+++ src/components/station/SubwayStationList.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './SubwayStationList.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { subwayStationIndex } from '../../store/thunks/subwayStationListThunk.js';
 
@@ -12,41 +12,49 @@ function SubwayStationList() {
   const onChange = (e) => {
     setSearch(e.target.value)
   }
-  
+
+
+
   const subwayList = useSelector(state => state.subwayStation.subwayList);
+
+
   useEffect(() => {
     dispatch(subwayStationIndex());
   }, []);
 
+  //use 메모사용해보기
+const filteredList = useMemo(() => {
+  const searchText = search.trim();
 
-let filteredList = [];
-  if(subwayList){
-    filteredList = subwayList.filter((item)=>{
-      const stationName = item.STATION_NM.trim();
-      const lineNum = item.LINE_NUM.replace(/^0+/,'');
-      const searchText = search.trim();
-      return (stationName.includes(searchText)||lineNum.includes(searchText))
-    })
-  }
+  return subwayList.filter((item) => {
+    const stationName = item.STATION_NM.trim();
+    const lineNum = item.LINE_NUM.replace(/^0+/, '');
+    return (
+      stationName.includes(searchText) ||
+      lineNum.includes(searchText)
+    );
+  });
+}, [subwayList, search]);
 
-  
+
+
   return (
     <>
       <div className="subway-station-list__title">
         <h1>지하철역 리스트</h1>
       </div>
       <div className="subway-station-list__searchbar">
-        <input type='text' value={search} onChange={onChange} placeholder="지하철역을 검색해주세요."/>
+        <input type='text' value={search} onChange={onChange} placeholder="지하철역을 검색해주세요." />
       </div>
       <div className="subway-station-list">
         {
           filteredList.map((item) => {
-            return(
+            return (
               <div className="subway-station-list__item" key={item.STATION_CD} onClick={() => { navigate(`/stations/${item.STATION_CD}/${item.LINE_NUM}/${item.STATION_NM}`) }}>
                 <div className="subway-station-list__icon">
                   <img src="/base/mainnavsubway.png" alt="지하철 사진" />
                 </div>
-                <p>{`${item.LINE_NUM.replace(/^0+/,'')} ${item.STATION_NM}`}</p>
+                <p>{`${item.LINE_NUM.replace(/^0+/, '')} ${item.STATION_NM}`}</p>
               </div>
             );
           })
